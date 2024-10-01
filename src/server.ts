ef601393d730ec8f3130d948f51c1e8f1acc15b0
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-10-01 12:54:46 -0300

    fix: fixing version from packageJson & including express in the server

--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,26 @@
-import { graphql, buildSchema } from "graphql"
- 
-// Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
+import express from 'express';
+import { graphqlHTTP } from 'express-graphql';
+import { buildSchema } from 'graphql';
+
+const app = express();
+
+const schema = buildSchema(`
   type Query {
     hello: String
   }
-`)
- 
-// The rootValue provides a resolver function for each API endpoint
-var rootValue = {
-  hello() {
-    return "Hello world!"
-  }
-}
- 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql({
-  schema,
-  source: "{ hello }",
-  rootValue
-}).then(response => {
-  console.log(response)
-})
+`);
+
+const rootValue = {
+  hello: () => 'Hello world!',
+};
+
+app.use('/graphql', graphqlHTTP({
+  schema: schema,
+  rootValue: rootValue,
+  graphiql: true, // Interface que vem do graphQL
+}));
+
+// Porta do servidor
+app.listen(4000, () => {
+  console.log('Servidor GraphQL rodando em http://localhost:4000/graphql');
+});
