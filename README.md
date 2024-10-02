2eb7201677a51444fdb6bcc0a6fc48624d9e95b9
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-10-01 23:26:24 -0300

    chore: apollo, typescript and inital configurations for prettier & eslint

--- README.md
+++ README.md
@@ -4,5 +4,4 @@ Um projeto backend focado na execução do desafio proposto pela instaQ
 
 ## Ambiente e ferramentas:
 
-
 ## Etapas para executar e depurar
---
+++ src/server.ts
@@ -0,0 +1,23 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+
+const typeDefs = `#graphql
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world!",
+  },
+};
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server).then(({ url }) => {
+  console.log(`Server ready at ${url}`);
+});
