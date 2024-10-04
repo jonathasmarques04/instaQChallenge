505ba4cfb81dac213cb468d88f8fd91b5e8e9040
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-10-04 13:25:33 -0300

    feat: createUser mutation & new eslint configuration

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,56 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+interface UserInput {
+  name: string;
+  email: string;
+  password: string;
+  birthDate: string;
+}
+
+interface User {
+  id: number;
+  name: string;
+  email: string;
+  birthDate: string;
+}
+
 const typeDefs = `#graphql
   type Query {
     hello: String
+  }
+
+  type User {
+    id: ID!
+    name: String! 
+    email: String!
+    birthDate: String!
+  }
+
+  input UserInput {
+    name: String! 
+    email: String!
+    password: String!
+    birthDate: String!
+  }
+
+  type Mutation {
+    createUser(data: UserInput!): User!
   }
 `;
 
 const resolvers = {
   Query: {
-    hello: () => "Hello world!",
+    hello: () => "Hello World",
   },
+  Mutation: {
+    createUser: (_: any,{ data }: {data: UserInput}): User => ({
+      id:  Math.floor(Math.random() * 1000), // Simula um id
+      name: data.name,
+      email: data.email,
+      birthDate: data.birthDate
+    })
+  }
 };
 
 const server = new ApolloServer({
