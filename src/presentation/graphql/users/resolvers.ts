93a424fdcd9db563d5ad1ba59dba1823b05d08ac
jonathas.marques102030 <jonathascraft102030gtav@gmail.com>
2024-10-07 20:47:43 -0300

    chore: added verification for email and password

--- src/presentation/graphql/users/resolvers.ts
+++ src/presentation/graphql/users/resolvers.ts
@@ -15,6 +15,20 @@ export default {
     createUser: async (_: any, { data }: UserInput) => {
       const { name, email, password, birthDate } = data;
 
+      if (password.length < 6 || !/[a-zA-Z]/.test(password) || !/[0-9]/.test(password)) {
+        throw new Error('A senha deve ter pelo menos 6 caracteres, incluindo pelo menos uma letra e um dígito.');
+      }
+
+      const emailVerify = await prisma.user.findFirst({
+        where: {
+            email: email
+        }
+      })
+
+      if(emailVerify){
+        throw new Error("Email already exists")
+      }
+
       const newUser = await prisma.user.create({
         data: {
           name,
